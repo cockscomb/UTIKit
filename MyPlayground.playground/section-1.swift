a5e2e0258d2e5efe566e830cb3f6de728ddf9d18
banjun <banjun@gmail.com>
2015-06-16 22:29:41 +0900

    convert to Swift 2

--- MyPlayground.playground/section-1.swift
+++ MyPlayground.playground/section-1.swift
@@ -8,9 +8,9 @@ UTI(MIMEType: "image/jpeg").filenameExtensions
 
 UTI(filenameExtension: "jpeg") == UTI(MIMEType: "image/jpeg")
 
-switch UTI(kUTTypeJPEG) {
-case UTI(kUTTypeImage):
-    println("JPEG is a kind of images")
+switch UTI(kUTTypeJPEG as String) {
+case UTI(kUTTypeImage as String):
+    print("JPEG is a kind of images")
 default:
     fatalError("JPEG must be a image")
 }
