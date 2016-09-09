2b2ad035f27267525668cf7a2077840d41968e12
cockscomb <cockscomb@hatena.ne.jp>
2016-09-09 17:29:14 +0900

    Adopt new naming guidelines

--- UTIKit/UTI.swift
+++ UTIKit/UTI.swift
@@ -28,116 +28,116 @@ import MobileCoreServices
 
 public struct UTI: CustomStringConvertible, CustomDebugStringConvertible, Equatable {
 
-    public let UTIString: String
+    public let utiString: String
 
     // MARK: - Initialize
 
-    public init(_ UTIString: String) {
-        self.UTIString = UTIString
+    public init(_ utiString: String) {
+        self.utiString = utiString
     }
 
-    public init?(filenameExtension: String, conformingToUTI: UTI? = nil) {
-        let conformingToUTIString: CFString? = conformingToUTI?.UTIString as CFString?
-        guard let rawUTI = UTTypeCreatePreferredIdentifierForTag(kUTTagClassFilenameExtension, filenameExtension as CFString, conformingToUTIString)?.takeRetainedValue() else { return nil }
-        self.UTIString = rawUTI as String
+    public init?(filenameExtension: String, conformingTo: UTI? = nil) {
+        let conformingToUtiString: CFString? = conformingTo?.utiString as CFString?
+        guard let rawUti = UTTypeCreatePreferredIdentifierForTag(kUTTagClassFilenameExtension, filenameExtension as CFString, conformingToUtiString)?.takeRetainedValue() else { return nil }
+        self.utiString = rawUti as String
     }
 
-    public init?(MIMEType: String, conformingToUTI: UTI? = nil) {
-        let conformingToUTIString: CFString? = conformingToUTI?.UTIString as CFString?
-        guard let rawUTI = UTTypeCreatePreferredIdentifierForTag(kUTTagClassMIMEType, MIMEType as CFString, conformingToUTIString)?.takeRetainedValue() else { return nil }
-        self.UTIString = rawUTI as String
+    public init?(mimeType: String, conformingTo: UTI? = nil) {
+        let conformingToUtiString: CFString? = conformingTo?.utiString as CFString?
+        guard let rawUti = UTTypeCreatePreferredIdentifierForTag(kUTTagClassMIMEType, mimeType as CFString, conformingToUtiString)?.takeRetainedValue() else { return nil }
+        self.utiString = rawUti as String
     }
 
     #if os(macOS)
-    public init?(pasteBoardType: String, conformingToUTI: UTI? = nil) {
-        let conformingToUTIString: CFString? = conformingToUTI?.UTIString
-        guard let rawUTI = UTTypeCreatePreferredIdentifierForTag(kUTTagClassNSPboardType, pasteBoardType, conformingToUTIString)?.takeRetainedValue() else { return nil }
-        self.UTIString = rawUTI as String
+    public init?(pasteBoardType: String, conformingTo: UTI? = nil) {
+        let conformingToUtiString: CFString? = conformingTo?.utiString as CFString?
+        guard let rawUti = UTTypeCreatePreferredIdentifierForTag(kUTTagClassNSPboardType, pasteBoardType as CFString, conformingToUtiString)?.takeRetainedValue() else { return nil }
+        self.utiString = rawUti as String
     }
 
-    public init?(OSType: String, conformingToUTI: UTI? = nil) {
-        let conformingToUTIString: CFString? = conformingToUTI?.UTIString
-        guard let rawUTI = UTTypeCreatePreferredIdentifierForTag(kUTTagClassOSType, OSType, conformingToUTIString)?.takeRetainedValue() else { return nil }
-        self.UTIString = rawUTI as String
+    public init?(OSType: String, conformingTo: UTI? = nil) {
+        let conformingToUtiString: CFString? = conformingTo?.utiString as CFString?
+        guard let rawUti = UTTypeCreatePreferredIdentifierForTag(kUTTagClassOSType, OSType as CFString, conformingToUtiString)?.takeRetainedValue() else { return nil }
+        self.utiString = rawUti as String
     }
     #endif
 
     // MARK: -
 
-    private static func UTIsForTagClass(_ tagClass: String, tag: String, conformingToUTI: UTI?) -> [UTI] {
-        let conformingToUTIString: CFString? = conformingToUTI?.UTIString as CFString?
+    private static func UTIs(for tagClass: String, tag: String, conformingTo: UTI?) -> [UTI] {
+        let conformingToUTIString: CFString? = conformingTo?.utiString as CFString?
         guard let rawUTIs = UTTypeCreateAllIdentifiersForTag(tagClass as CFString, tag as CFString, conformingToUTIString)?.takeRetainedValue() else { return [] }
         return (rawUTIs as NSArray as? [String] ?? []).map { UTI($0) }
     }
 
-    public static func UTIsFromFilenameExtension(_ filenameExtension: String, conformingToUTI: UTI? = nil) -> [UTI] {
-        return UTIsForTagClass(kUTTagClassFilenameExtension as String, tag: filenameExtension, conformingToUTI: conformingToUTI)
+    public static func UTIs(fromFilenameExtension filenameExtension: String, conformingTo: UTI? = nil) -> [UTI] {
+        return UTIs(for: kUTTagClassFilenameExtension as String, tag: filenameExtension, conformingTo: conformingTo)
     }
 
-    public static func UTIsFromMIMEType(_ MIMEType: String, conformingToUTI: UTI? = nil) -> [UTI] {
-        return UTIsForTagClass(kUTTagClassMIMEType as String, tag: MIMEType, conformingToUTI: conformingToUTI)
+    public static func UTIs(fromMimeType mimeType: String, conformingTo: UTI? = nil) -> [UTI] {
+        return UTIs(for: kUTTagClassMIMEType as String, tag: mimeType, conformingTo: conformingTo)
     }
 
     #if os(macOS)
-    public static func UTIsFromPasteBoardType(pasteBoardType: String, conformingToUTI: UTI? = nil) -> [UTI] {
-        return UTIsForTagClass(kUTTagClassNSPboardType as String, tag: pasteBoardType, conformingToUTI: conformingToUTI)
+    public static func UTIs(fromPasteBoardType pasteBoardType: String, conformingTo: UTI? = nil) -> [UTI] {
+        return UTIs(for: kUTTagClassNSPboardType as String, tag: pasteBoardType, conformingTo: conformingTo)
     }
 
-    public static func UTIsFromOSType(OSType: String, conformingToUTI: UTI? = nil) -> [UTI] {
-        return UTIsForTagClass(kUTTagClassOSType as String, tag: OSType, conformingToUTI: conformingToUTI)
+    public static func UTIs(fromOTType OSType: String, conformingTo: UTI? = nil) -> [UTI] {
+        return UTIs(for: kUTTagClassOSType as String, tag: OSType, conformingTo: conformingTo)
     }
     #endif
 
     // MARK: - Tags
 
-    private func tagWithClass(_ tagClass: String) -> String? {
-        return UTTypeCopyPreferredTagWithClass(UTIString as CFString, tagClass as CFString)?.takeRetainedValue() as String?
+    private func tag(withClass tagClass: String) -> String? {
+        return UTTypeCopyPreferredTagWithClass(utiString as CFString, tagClass as CFString)?.takeRetainedValue() as String?
     }
 
     @available(macOS, introduced:10.10)
     @available(iOS, introduced:8.0)
-    private func tagsWithClass(_ tagClass: String) -> [String] {
-        guard let tags = UTTypeCopyAllTagsWithClass(UTIString as CFString, tagClass as CFString)?.takeRetainedValue() else { return [] }
+    private func tags(withClass tagClass: String) -> [String] {
+        guard let tags = UTTypeCopyAllTagsWithClass(utiString as CFString, tagClass as CFString)?.takeRetainedValue() else { return [] }
         return tags as NSArray as? [String] ?? []
     }
 
     public var filenameExtension: String? {
-        return tagWithClass(kUTTagClassFilenameExtension as String)
+        return tag(withClass: kUTTagClassFilenameExtension as String)
     }
 
     @available(macOS, introduced:10.10)
     @available(iOS, introduced:8.0)
     public var filenameExtensions: [String] {
-        return tagsWithClass(kUTTagClassFilenameExtension as String)
+        return tags(withClass: kUTTagClassFilenameExtension as String)
     }
 
-    public var MIMEType: String? {
-        return tagWithClass(kUTTagClassMIMEType as String)
+    public var mimeType: String? {
+        return tag(withClass: kUTTagClassMIMEType as String)
     }
 
     @available(macOS, introduced:10.10)
     @available(iOS, introduced:8.0)
-    public var MIMETypes: [String] {
-        return tagsWithClass(kUTTagClassMIMEType as String)
+    public var mimeTypes: [String] {
+        return tags(withClass: kUTTagClassMIMEType as String)
     }
 
     #if os(macOS)
     public var pasteBoardType: String? {
-        return tagWithClass(kUTTagClassNSPboardType as String)
+        return tag(withClass: kUTTagClassNSPboardType as String)
     }
 
     @available(macOS, introduced:10.10)
     public var pasteBoardTypes: [String] {
-        return tagsWithClass(kUTTagClassNSPboardType as String)
+        return tags(withClass: kUTTagClassNSPboardType as String)
     }
 
     public var OSType: String? {
-        return tagWithClass(kUTTagClassOSType as String)
+        return tag(withClass: kUTTagClassOSType as String)
     }
 
     @available(macOS, introduced:10.10)
     public var OSTypes: [String] {
-        return tagsWithClass(kUTTagClassOSType as String)
+        return tags(withClass: kUTTagClassOSType as String)
     }
     #endif
 
@@ -146,38 +146,38 @@ public struct UTI: CustomStringConvertible, CustomDebugStringConvertible, Equata
     @available(macOS, introduced:10.10)
     @available(iOS, introduced:8.0)
     public var isDeclared: Bool {
-        return UTTypeIsDeclared(UTIString as CFString)
+        return UTTypeIsDeclared(utiString as CFString)
     }
 
     @available(macOS, introduced:10.10)
     @available(iOS, introduced:8.0)
     public var isDynamic: Bool {
-        return UTTypeIsDynamic(UTIString as CFString)
+        return UTTypeIsDynamic(utiString as CFString)
     }
 
     // MARK: - Declaration
 
     public struct Declaration: CustomStringConvertible, CustomDebugStringConvertible {
-        private let raw: [ NSObject: AnyObject ]
+        fileprivate let raw: [AnyHashable: Any]
 
         public var exportedTypeDeclarations: [Declaration] {
-            return (raw[kUTExportedTypeDeclarationsKey] as? [[ NSObject : AnyObject ]] ?? []).map { Declaration(declaration: $0) }
+            return (raw[kUTExportedTypeDeclarationsKey as AnyHashable] as? [[AnyHashable: Any]] ?? []).map { Declaration(declaration: $0) }
         }
 
         public var importedTypeDeclarations: [Declaration] {
-            return (raw[kUTImportedTypeDeclarationsKey] as? [[ NSObject : AnyObject ]] ?? []).map { Declaration(declaration: $0) }
+            return (raw[kUTImportedTypeDeclarationsKey as AnyHashable] as? [[AnyHashable: Any]] ?? []).map { Declaration(declaration: $0) }
         }
 
         public var identifier: String? {
-            return raw[kUTTypeIdentifierKey] as? String
+            return raw[kUTTypeIdentifierKey as AnyHashable] as? String
         }
 
-        public var tagSpecification: [ NSObject : AnyObject ] {
-            return raw[kUTTypeTagSpecificationKey] as? [ NSObject : AnyObject ] ?? [:]
+        public var tagSpecification: [AnyHashable: Any] {
+            return raw[kUTTypeTagSpecificationKey as AnyHashable] as? [AnyHashable: Any] ?? [:]
         }
 
         public var conformsTo: [UTI] {
-            switch raw[kUTTypeConformsToKey] {
+            switch raw[kUTTypeConformsToKey as AnyHashable] {
             case let array as [String]:
                 return array.map { UTI($0) }
             case let string as String:
@@ -188,21 +188,21 @@ public struct UTI: CustomStringConvertible, CustomDebugStringConvertible, Equata
         }
 
         public var iconFile: String? {
-            return raw[kUTTypeIconFileKey] as? String
+            return raw[kUTTypeIconFileKey as AnyHashable] as? String
         }
 
-        public var referenceURL: URL? {
-            if let reference = raw[kUTTypeReferenceURLKey] as? String {
+        public var referenceUrl: URL? {
+            if let reference = raw[kUTTypeReferenceURLKey as AnyHashable] as? String {
                 return URL(string: reference)
             }
             return nil
         }
 
         public var version: String? {
-            return raw[kUTTypeIconFileKey] as? String
+            return raw[kUTTypeIconFileKey as AnyHashable] as? String
         }
 
-        init(declaration: [ NSObject : AnyObject ]) {
+        init(declaration: [AnyHashable: Any]) {
             self.raw = declaration
         }
 
@@ -217,17 +217,17 @@ public struct UTI: CustomStringConvertible, CustomDebugStringConvertible, Equata
     }
 
     public var declaration: Declaration {
-        return Declaration(declaration: UTTypeCopyDeclaration(self.UTIString as CFString)?.takeRetainedValue() as [NSObject: AnyObject]? ?? [:])
+        return Declaration(declaration: UTTypeCopyDeclaration(self.utiString as CFString)?.takeRetainedValue() as! [AnyHashable: Any]? ?? [:])
     }
 
     public var declaringBundle: Bundle? {
-        if let url = UTTypeCopyDeclaringBundleURL(UTIString as CFString)?.takeRetainedValue() {
+        if let url = UTTypeCopyDeclaringBundleURL(utiString as CFString)?.takeRetainedValue() {
             return Bundle(url: url as URL)
         }
         return nil
     }
 
-    public var iconFileURL: URL? {
+    public var iconFileUrl: URL? {
         if let iconFile = declaration.iconFile {
             return self.declaringBundle?.url(forResource: iconFile, withExtension: nil) ??
                    self.declaringBundle?.url(forResource: iconFile, withExtension: "icns")
@@ -238,19 +238,96 @@ public struct UTI: CustomStringConvertible, CustomDebugStringConvertible, Equata
     // MARK: - Printable, DebugPrintable
 
     public var description: String {
-        return UTTypeCopyDescription(UTIString as CFString)?.takeRetainedValue() as? String ?? UTIString
+        return UTTypeCopyDescription(utiString as CFString)?.takeRetainedValue() as? String ?? utiString
     }
 
     public var debugDescription: String {
-        return UTIString
+        return utiString
     }
 
 }
 
 public func ==(lhs: UTI, rhs: UTI) -> Bool {
-    return UTTypeEqual(lhs.UTIString as CFString, rhs.UTIString as CFString)
+    return UTTypeEqual(lhs.utiString as CFString, rhs.utiString as CFString)
 }
 
 public func ~=(pattern: UTI, value: UTI) -> Bool {
-    return UTTypeConformsTo(value.UTIString as CFString, pattern.UTIString as CFString)
+    return UTTypeConformsTo(value.utiString as CFString, pattern.utiString as CFString)
+}
+
+// MARK: - Deprecated
+
+public extension UTI {
+
+    @available(*, unavailable, renamed: "utiString")
+    public var UTIString: String {
+        return utiString
+    }
+
+    @available(*, unavailable, renamed: "init(filenameExtension:conformingTo:)")
+    public init?(filenameExtension: String, conformingToUTI: UTI? = nil) {
+        self.init(filenameExtension: filenameExtension, conformingTo: conformingToUTI)
+    }
+
+    @available(*, unavailable, renamed: "init(mimeType:conformingTo:)")
+    public init?(MIMEType: String, conformingToUTI: UTI? = nil) {
+        self.init(mimeType: MIMEType, conformingTo: conformingToUTI)
+    }
+
+    @available(*, unavailable, renamed: "UTIs(fromFilenameExtension:conformingTo:)")
+    public static func UTIsFromFilenameExtension(_ filenameExtension: String, conformingToUTI: UTI? = nil) -> [UTI] {
+        return UTIs(fromFilenameExtension: filenameExtension, conformingTo: conformingToUTI)
+    }
+
+    @available(*, unavailable, renamed: "UTIs(fromMimeType:conformingTo:)")
+    public static func UTIsFromMIMEType(_ MIMEType: String, conformingToUTI: UTI? = nil) -> [UTI] {
+        return UTIs(fromMimeType: MIMEType, conformingTo: conformingToUTI)
+    }
+
+    #if os(macOS)
+    @available(*, unavailable, renamed: "init(pasteBoardType:conformingTo:)")
+    public init?(pasteBoardType: String, conformingToUTI: UTI? = nil) {
+        self.init(pasteBoardType: pasteBoardType, conformingTo: conformingToUTI)
+    }
+
+    @available(*, unavailable, renamed: "init(OSType:conformingTo:)")
+    public init?(OSType: String, conformingToUTI: UTI? = nil) {
+        self.init(OSType: OSType, conformingTo: conformingToUTI)
+    }
+
+    @available(*, unavailable, renamed: "UTIs(fromPasteBoardType:conformingTo:)")
+    public static func UTIsFromPasteBoardType(pasteBoardType: String, conformingToUTI: UTI? = nil) -> [UTI] {
+        return UTIs(fromPasteBoardType: pasteBoardType, conformingTo: conformingToUTI)
+    }
+
+    @available(*, unavailable, renamed: "UTIs(fromOSType:conformingTo:)")
+    public static func UTIsFromOSType(OSType: String, conformingToUTI: UTI? = nil) -> [UTI] {
+        return UTIs(fromOTType: OSType, conformingTo: conformingToUTI)
+    }
+    #endif
+
+    @available(*, unavailable, renamed: "mimeType")
+    public var MIMEType: String? {
+        return mimeType
+    }
+
+    @available(*, unavailable, renamed: "mimeTypes")
+    public var MIMETypes: [String] {
+        return mimeTypes
+    }
+
+    @available(*, unavailable, renamed: "iconFileUrl")
+    public var iconFileURL: URL? {
+        return iconFileUrl
+    }
+
+}
+
+public extension UTI.Declaration {
+
+    @available(*, unavailable, renamed: "referenceUrl")
+    public var referenceURL: URL? {
+        return referenceUrl
+    }
+
 }
