c7543ef2a6a2639952ccd02f275d00277513738b
Tony Osibov <tony.osibov@gmail.com>
2020-12-21 15:43:31 -0800

    added swift package manager file

---
+++ Package.swift
@@ -0,0 +1,15 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "UTIKit",
+    products: [
+        .library(name: "UTIKit", targets: ["UTIKit"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(name: "UTIKit", dependencies: [], path: "UTIKit")
+    ]
+)
